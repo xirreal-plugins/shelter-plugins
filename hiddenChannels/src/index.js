5cb617e41e767274fe6f8ebf42c2c5024b1dfac8
alethéia <34745171+xirreal@users.noreply.github.com>
2022-09-03 11:41:43 +0200

    Oop 2

--- hiddenChannels/src/index.js
+++ hiddenChannels/src/index.js
@@ -103,7 +103,7 @@ export default (data) => {
             });
 
             unpatchList.unreadStateManager = patcher.after("canTrackUnreads", unreadStateManager, (originalArgs, previousReturn) => {
-                return previousReturn && Channel.prototype.canBeSeen(originalArgs[0]);
+                return previousReturn && getChannel(originalArgs[0])?.canBeSeen();
             });
 
             unpatchList.fetchMessages = patcher.instead("fetchMessages", fetchMessages, (originalArgs, originalFunction) => {
