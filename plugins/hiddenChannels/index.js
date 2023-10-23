3e544755138ad7faab0d44486ff477a4e6a35bdf
alethéia <me@xirreal.dev>
2023-10-23 09:38:42 +0200

    [hiddenChannels] fix colored icons

--- plugins/hiddenChannels/index.js
+++ plugins/hiddenChannels/index.js
@@ -1,38 +1,38 @@
 const {
-  patcher,
-  observeDom,
-  flux: {
-    intercept,
-    dispatcher,
-    stores: {
-      PermissionStore,
-      ChannelStore,
-      VoiceStateStore,
-      GuildStore,
-      ReadStateStore,
-    },
-  },
-  ui: { injectCss, renderSolidInReact },
-  util: { getFiber, reactFiberWalker },
+   patcher,
+   observeDom,
+   flux: {
+      intercept,
+      dispatcher,
+      stores: {
+         PermissionStore,
+         ChannelStore,
+         VoiceStateStore,
+         GuildStore,
+         ReadStateStore,
+      },
+   },
+   ui: { injectCss, renderSolidInReact },
+   util: { getFiber, reactFiberWalker },
 } = shelter;
 
 const Permissions = {
-  VIEW_CHANNEL: 1024n,
+   VIEW_CHANNEL: 1024n,
 };
 
 const ChannelTypes = {
-  GUILD_TEXT: 0,
-  DM: 1,
-  GUILD_VOICE: 2,
-  GROUP_DM: 3,
-  GUILD_CATEGORY: 4,
-  GUILD_ANNOUNCEMENT: 5,
-  ANNOUNCEMENT_THREAD: 10,
-  PUBLIC_THREAD: 11,
-  PRIVATE_THREAD: 12,
-  GUILD_STAGE_VOICE: 13,
-  GUILD_DIRECTORY: 14,
-  GUILD_FORUM: 15,
+   GUILD_TEXT: 0,
+   DM: 1,
+   GUILD_VOICE: 2,
+   GROUP_DM: 3,
+   GUILD_CATEGORY: 4,
+   GUILD_ANNOUNCEMENT: 5,
+   ANNOUNCEMENT_THREAD: 10,
+   PUBLIC_THREAD: 11,
+   PRIVATE_THREAD: 12,
+   GUILD_STAGE_VOICE: 13,
+   GUILD_DIRECTORY: 14,
+   GUILD_FORUM: 15,
 };
 
 const getChannel = ChannelStore.getChannel;
@@ -49,182 +49,193 @@ const unpatchList = {};
 let headerBar = null;
 
 function canBeSeen(channel) {
-  return originalCan(Permissions.VIEW_CHANNEL, channel);
+   return originalCan(Permissions.VIEW_CHANNEL, channel);
 }
 
 const isRestrictedChannel = (channel) => {
-  return channel?.permissionOverwrites?.length > 0;
+   return channel?.permissionOverwrites?.length > 0;
 };
 
 const isVisibile = (channel) => {
-  if (typeof channel !== "object" && !channel?.id) {
-    try {
-      channel = getChannel(channel);
-    } catch {
-      channel = null;
-    } // Just in case Discord fucks up permissions again, so it doesn't crash.
-  }
+   if (typeof channel !== "object" && !channel?.id) {
+      try {
+         channel = getChannel(channel);
+      } catch {
+         channel = null;
+      } // Just in case Discord fucks up permissions again, so it doesn't crash.
+   }
 
-  // Exclude DM channels or invalid channels
-  if (
-    !channel ||
-    [
-      ChannelTypes.DM,
-      ChannelTypes.GROUP_DM,
-      ChannelTypes.GUILD_CATEGORY,
-      ChannelTypes.GUILD_STORE,
-      ChannelTypes.GUILD_DIRECTORY,
-    ].includes(channel.type)
-  )
-    return true;
+   // Exclude DM channels or invalid channels
+   if (
+      !channel ||
+      [
+         ChannelTypes.DM,
+         ChannelTypes.GROUP_DM,
+         ChannelTypes.GUILD_CATEGORY,
+         ChannelTypes.GUILD_STORE,
+         ChannelTypes.GUILD_DIRECTORY,
+      ].includes(channel.type)
+   )
+      return true;
 
-  return canBeSeen(channel);
+   return canBeSeen(channel);
 };
 
 export function onLoad() {
-  unpatchList.css = injectCss(css);
+   unpatchList.css = injectCss(css);
 
-  unpatchList.can = patcher.instead(
-    "can",
-    PermissionStore.__proto__,
-    (originalArgs, originalFunction) => {
-      if (
-        originalArgs[0] == Permissions.VIEW_CHANNEL &&
-        !isRestrictedChannel(originalArgs[1])
-      )
-        return true;
+   unpatchList.can = patcher.instead(
+      "can",
+      PermissionStore.__proto__,
+      (originalArgs, originalFunction) => {
+         if (
+            originalArgs[0] == Permissions.VIEW_CHANNEL &&
+            !isRestrictedChannel(originalArgs[1])
+         )
+            return true;
 
-      return originalFunction(...originalArgs);
-    }
-  );
+         return originalFunction(...originalArgs);
+      }
+   );
 
-  let stopObservingChannelItem = observeDom(
-    '[data-list-item-id^="channels___"',
-    (element) => {
-      queueMicrotask(() => {
-        const channelId = element.dataset.listItemId.split("___")[1];
-        if (isNaN(parseInt(channelId))) return;
-        const component = reactFiberWalker(
-          getFiber(element),
-          "aria-label",
-          true,
-          true
-        )?.type;
-        if (!component || typeof component.render !== "function") return;
+   unpatchList.stopObservingChannelItem = observeDom(
+      '[data-list-item-id^="channels___"',
+      (element) => {
+         queueMicrotask(() => {
+            const channelId = element.dataset.listItemId.split("___")[1];
+            if (isNaN(parseInt(channelId))) return;
 
-        stopObservingChannelItem();
+            const component = reactFiberWalker(
+               getFiber(element),
+               "aria-label",
+               true,
+               true
+            )?.type;
 
-        if (unpatchList.channelItem) {
-          return;
-        }
+            if (!component || typeof component.render !== "function") return;
 
-        unpatchList.channelItem = patcher.before(
-          "render",
-          component,
-          (originalArgs) => {
-            // Find if the element has a child with the channel prop
-            for (let i = 0; i < originalArgs[0].children.length; i++) {
-              if (!isVisibile(originalArgs[0].children[i]?.props?.channel)) {
-                originalArgs[0]["aria-label"] += " hidden";
-                break;
-              }
+            unpatchList.stopObservingChannelItem();
+
+            if (unpatchList.channelItem) {
+               return;
             }
 
-            return originalArgs;
-          }
-        );
+            unpatchList.channelItem = patcher.before(
+               "render",
+               component,
+               (originalArgs) => {
+                  const channelId =
+                     originalArgs[0]["data-list-item-id"].split("___")[1];
 
-        const channelReadState = ReadStateStore.getForDebugging(channelId);
-        unpatchList.unreadStateManager = patcher.after(
-          "canTrackUnreads",
-          channelReadState.__proto__,
-          function (_, previousReturn) {
-            return previousReturn && isVisibile(this.channelId);
-          }
-        );
-      });
-    }
-  );
+                  if (!isVisibile(channelId)) {
+                     originalArgs[0].className += " shelter_hiddenChannel";
+                  }
 
-  let stopObservingHeaderBar = observeDom("[class^=title-]", (element) => {
-    queueMicrotask(() => {
-      const component = reactFiberWalker(
-        getFiber(element),
-        "toolbar",
-        true,
-        true
-      )?.type;
-      if (!component || typeof component.Icon !== "function" || headerBar)
-        return;
-      stopObservingHeaderBar();
-      headerBar = component;
-    });
-  });
+                  return originalArgs;
+               }
+            );
 
-  let stopObservingRoute = observeDom('[class^="chat-"]', (element) => {
-    queueMicrotask(() => {
-      const component = reactFiberWalker(
-        getFiber(element),
-        "computedMatch",
-        true,
-        true
-      )?.type;
-      if (!component || typeof component.prototype.render !== "function")
-        return;
+            const channelReadState = ReadStateStore.getForDebugging(channelId);
+            unpatchList.unreadStateManager = patcher.after(
+               "canTrackUnreads",
+               channelReadState.__proto__,
+               function (_, previousReturn) {
+                  return previousReturn && isVisibile(this.channelId);
+               }
+            );
+         });
+      }
+   );
 
-      stopObservingRoute();
+   unpatchList.stopObservingHeaderBar = observeDom(
+      "[class^=title-]",
+      (element) => {
+         queueMicrotask(() => {
+            const component = reactFiberWalker(
+               getFiber(element),
+               "toolbar",
+               true,
+               true
+            )?.type;
+            if (!component || typeof component.Icon !== "function" || headerBar)
+               return;
+            unpatchList.stopObservingHeaderBar();
+            headerBar = component;
+         });
+      }
+   );
 
-      if (unpatchList.Route) return;
+   unpatchList.stopObservingRoute = observeDom(
+      '[class^="chat-"]',
+      (element) => {
+         queueMicrotask(() => {
+            const component = reactFiberWalker(
+               getFiber(element),
+               "computedMatch",
+               true,
+               true
+            )?.type;
+            if (!component || typeof component.prototype.render !== "function")
+               return;
 
-      unpatchList.Route = patcher.before(
-        "render",
-        component.prototype,
-        function (originalArgs) {
-          if (this.props?.path?.length !== 3) return originalArgs;
+            unpatchList.stopObservingRoute();
 
-          const channelId = this.props?.computedMatch?.params?.channelId;
-          const guildId = this.props?.computedMatch?.params?.guildId;
+            if (unpatchList.Route) return;
 
-          if (
-            !isVisibile(channelId) &&
-            guildId &&
-            headerBar !== null
-            // && (isInVoice() ? channelId != getVoiceID(guildId) : true) TODO: Actually fix!!!
-          ) {
-            this.props.render = () => {
-              return renderSolidInReact(Notice, {
-                channel: getChannel(channelId),
-                guild: getGuild(guildId),
-                components: {
-                  headerBar,
-                },
-              });
-            };
-          }
+            unpatchList.Route = patcher.before(
+               "render",
+               component.prototype,
+               function (originalArgs) {
+                  if (this.props?.path?.length !== 3) return originalArgs;
 
-          return originalArgs;
-        }
-      );
-    });
-  });
+                  const channelId =
+                     this.props?.computedMatch?.params?.channelId;
+                  const guildId = this.props?.computedMatch?.params?.guildId;
 
-  let shouldIgnoreNextMessageFetch = false;
-  let _channelId = "";
-  unpatchList.intercept = intercept((event) => {
-    if (event.type === "CHANNEL_SELECT" && !isVisibile(event?.channelId)) {
-      shouldIgnoreNextMessageFetch = true;
-      _channelId = event.channelId;
-    } else if (event.type !== "MESSAGE_FETCH" && shouldIgnoreNextMessageFetch) {
-      shouldIgnoreNextMessageFetch = false;
-      dispatcher.dispatch({
-        type: "LOAD_MESSAGES_FAILURE",
-        channelId: _channelId,
-      });
-      return [event, true];
-    }
-  });
+                  if (
+                     !isVisibile(channelId) &&
+                     guildId &&
+                     headerBar !== null
+                     // && (isInVoice() ? channelId != getVoiceID(guildId) : true) TODO: Actually fix!!!
+                  ) {
+                     this.props.render = () => {
+                        return renderSolidInReact(Notice, {
+                           channel: getChannel(channelId),
+                           guild: getGuild(guildId),
+                           components: {
+                              headerBar,
+                           },
+                        });
+                     };
+                  }
+
+                  return originalArgs;
+               }
+            );
+         });
+      }
+   );
+
+   let shouldIgnoreNextMessageFetch = false;
+   let _channelId = "";
+   unpatchList.intercept = intercept((event) => {
+      if (event.type === "CHANNEL_SELECT" && !isVisibile(event?.channelId)) {
+         shouldIgnoreNextMessageFetch = true;
+         _channelId = event.channelId;
+      } else if (
+         event.type !== "MESSAGE_FETCH" &&
+         shouldIgnoreNextMessageFetch
+      ) {
+         shouldIgnoreNextMessageFetch = false;
+         dispatcher.dispatch({
+            type: "LOAD_MESSAGES_FAILURE",
+            channelId: _channelId,
+         });
+         return [event, true];
+      }
+   });
 }
 
 export function onUnload() {
-  for (let unpatch of Object.values(unpatchList)) unpatch();
+   for (let unpatch of Object.values(unpatchList)) unpatch();
 }
