ce08887283af72a5fe23f1696b95f1e4277e7679
alethéia <me@xirreal.dev>
2023-10-23 10:18:35 +0200

    VC Timer

---
+++ plugins/vcTimer/index.jsx
@@ -0,0 +1,48 @@
+const {
+   flux: { dispatcher },
+   solid: { createSignal, onCleanup },
+} = shelter;
+
+function format(milliseconds) {
+   const hours = Math.floor(milliseconds / 3600000);
+   const minutes = Math.floor((milliseconds % 3600000) / 60000);
+   const seconds = Math.floor((milliseconds % 60000) / 1000);
+
+   return `${hours > 0 ? hours.padStart(2, "0") + ":" : ""}${minutes
+      .toString()
+      .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+}
+
+function Timer() {
+   const startTime = Date.now();
+   const [elapsed, update] = createSignal(0);
+
+   const timer = setInterval(() => {
+      update(Date.now() - startTime);
+   }, 1000);
+
+   onCleanup(() => clearInterval(timer));
+
+   return (
+      <div style={"font-weight: bold"}>
+         {format(elapsed())}
+         <br />
+      </div>
+   );
+}
+
+function onVoiceJoin(e) {
+   if (e.state != "RTC_CONNECTED") return;
+
+   document
+      .querySelector('[class^="rtcConnectionStatus-"] + a > div')
+      .prepend(<Timer />);
+}
+
+export function onLoad() {
+   dispatcher.subscribe("RTC_CONNECTION_STATE", onVoiceJoin);
+}
+
+export function onUnload() {
+   dispatcher.unsubscribe("RTC_CONNECTION_STATE", onVoiceJoin);
+}
