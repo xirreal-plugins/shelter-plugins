8a7dacf4229be622561fd02a82e26435f7e53b7f
xirreal <xirreal@users.noreply.github.com>
2024-12-29 13:09:23 +0000

    deploy: c654b4b37b99e5358ba62cf123e0e2aec7aaa090

---
+++ content-modules.mjs
@@ -0,0 +1,10 @@
+
+export default new Map([
+["src/plugins/external-uploader.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fplugins%2Fexternal-uploader.mdx&astroContentModuleFlag=true")],
+["src/plugins/favorite-gif-search.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fplugins%2Ffavorite-gif-search.mdx&astroContentModuleFlag=true")],
+["src/plugins/fweemojis.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fplugins%2Ffweemojis.mdx&astroContentModuleFlag=true")],
+["src/plugins/hidden-channels.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fplugins%2Fhidden-channels.mdx&astroContentModuleFlag=true")],
+["src/plugins/install-button.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fplugins%2Finstall-button.mdx&astroContentModuleFlag=true")],
+["src/plugins/soundboard-hotkeys.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fplugins%2Fsoundboard-hotkeys.mdx&astroContentModuleFlag=true")],
+["src/plugins/vc-timer.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fplugins%2Fvc-timer.mdx&astroContentModuleFlag=true")]]);
+		
