7eddf5422d7e2f668a8ae9597c97e3830e4bb4c2
xirreal <me@xirreal.dev>
2025-04-21 15:17:25 +0200

    Update external-upload for visual refresh

--- plugins/externalUpload/uploadIcon.jsx
+++ plugins/externalUpload/uploadIcon.jsx
@@ -4,8 +4,8 @@ export default function uploadIcon() {
          aria-hidden="true"
          role="img"
          xmlns="http://www.w3.org/2000/svg"
-         width="24"
-         height="24"
+         width="21"
+         height="21"
          fill="none"
          viewBox="0 0 24 24"
       >
