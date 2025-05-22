b4da02e888c63bab08ee914c7fe1542c014a9160
xirreal <me@xirreal.dev>
2025-05-22 09:18:03 +0200

    Fix favorite gif search, nuke macos files from git

--- plugins/favoriteSearch/index.jsx
+++ plugins/favoriteSearch/index.jsx
@@ -1,97 +1,110 @@
 const {
-   flux: { subscribe },
-   observeDom,
+  flux: { subscribe },
+  observeDom,
 } = shelter.plugin.scoped;
 
 const {
-   ui: { TextBox, openModal },
-   util: { getFiberOwner, getFiber, reactFiberWalker },
-   plugin: { store },
+  ui: { TextBox, openModal },
+  util: { getFiberOwner, getFiber, reactFiberWalker },
+  plugin: { store },
 } = shelter;
 
 import { AddTagModal } from "./modal";
 
 function addClickHandlerToFavoritesGifPicker() {
-   const stopObserving = observeDom(
-      "#gif-picker-tab-panel > div:last-child > div:first-child > div > div > div:first-child",
-      (container) => {
-         stopObserving();
-         if (container.dataset.clickHandlerAdded) return;
+  const stopObserving = observeDom(
+    "[class^='result'] > [class^='categoryFadeBlurple']",
+    (container) => {
+      stopObserving();
+      if (container.parentElement.dataset.clickHandlerAdded) return;
 
-         container.dataset.clickHandler = "true";
-         container.addEventListener("click", handleClick);
-      },
-   );
-   setTimeout(stopObserving, 500);
+      container.parentElement.dataset.clickHandler = "true";
+      container.parentElement.addEventListener("click", handleClick);
+    }
+  );
+  setTimeout(stopObserving, 500);
 }
 
 let stopObservingResults = null;
 function handleClick() {
-   const stopObserving = observeDom("#gif-picker-tab-panel > div:first-child", (header) => {
-      stopObserving();
-      const searchBox = (
-         <TextBox
-            id="gif-searchbox"
-            style={{ "margin-top": "8px" }}
-            placeholder="Search favorite GIFs..."
-            onInput={(text) => handleSearch(text)}
-         />
-      );
-      header.appendChild(searchBox);
+  requestAnimationFrame(() => {
+    const header = document.querySelector(
+      "#gif-picker-tab-panel > div:first-child"
+    );
+    if (!header) return;
+    const searchBox = (
+      <TextBox
+        id="gif-searchbox"
+        style={{ "margin-top": "8px" }}
+        placeholder="Search favorite GIFs..."
+        onInput={(text) => handleSearch(text)}
+      />
+    );
+    header.appendChild(searchBox);
 
-      stopObservingResults = observeDom("[class^='content'] > div > [class^='result']", (card) => {
-         if (card.dataset.addedRightClick) return;
-         card.dataset.addedRightClick = "true";
+    stopObservingResults = observeDom(
+      "[class^='content'] > div > [class^='result_']",
+      (card) => {
+        if (card.dataset.addedRightClick) return;
+        card.dataset.addedRightClick = "true";
 
-         addRightClickHandler(card);
-      });
-   });
-   setTimeout(stopObserving, 500);
+        addRightClickHandler(card);
+      }
+    );
+  });
 }
 
 function handleBack() {
-   document.getElementById("gif-searchbox")?.remove();
-   if (stopObservingResults) {
-      stopObservingResults();
-      stopObservingResults = null;
-   }
-   addClickHandlerToFavoritesGifPicker();
+  document.getElementById("gif-searchbox")?.remove();
+  if (stopObservingResults) {
+    stopObservingResults();
+    stopObservingResults = null;
+  }
+  addClickHandlerToFavoritesGifPicker();
 }
 
 function handleSearch(text) {
-   const container = document.querySelector("#gif-picker-tab-panel");
-   const fiber = getFiberOwner(container);
-   if (!fiber.props.__favorites) {
-      fiber.props.__favorites = fiber.props.favorites;
-   }
+  const container = document.querySelector("#gif-picker-tab-panel");
+  const fiber = getFiberOwner(container);
+  if (!fiber.props.__favorites) {
+    fiber.props.__favorites = fiber.props.favorites;
+  }
 
-   fiber.props.favorites = fiber.props.__favorites.filter((gif) => {
-      for (const tag of store[gif.url] ?? []) {
-         if (tag.toLowerCase().includes(text.toLowerCase())) {
-            return true;
-         }
+  fiber.props.favorites = fiber.props.__favorites.filter((gif) => {
+    for (const tag of store[gif.url] ?? []) {
+      if (tag.toLowerCase().includes(text.toLowerCase())) {
+        return true;
       }
+    }
 
-      return gif.url.toLowerCase().includes(text.toLowerCase());
-   });
+    return gif.url.toLowerCase().includes(text.toLowerCase());
+  });
 
-   fiber.forceUpdate();
+  fiber.forceUpdate();
 }
 
 function addRightClickHandler(container) {
-   container.addEventListener("contextmenu", (e) => {
-      openModal((p) =>
-         AddTagModal(p.close, reactFiberWalker(getFiber(e.target.parentNode), "item", true).memoizedProps.item),
-      );
-   });
+  container.addEventListener("contextmenu", (e) => {
+    console.log(
+      reactFiberWalker(getFiber(e.target.parentNode), "item", true)
+        .memoizedProps
+    );
+    openModal((p) =>
+      AddTagModal(
+        p.close,
+        reactFiberWalker(getFiber(e.target.parentNode), "item", true)
+          .memoizedProps.item
+      )
+    );
+  });
 }
 
 export function onLoad() {
-   subscribe("GIF_PICKER_INITIALIZE", (e) => {
-      addClickHandlerToFavoritesGifPicker();
-   });
+  subscribe("GIF_PICKER_INITIALIZE", (e) => {
+    addClickHandlerToFavoritesGifPicker();
+  });
 
-   subscribe("GIF_PICKER_QUERY", (e) => {
-      handleBack();
-   });
+  subscribe("GIF_PICKER_QUERY", (e) => {
+    handleBack();
+  });
 }
--- plugins/favoriteSearch/modal.jsx
+++ plugins/favoriteSearch/modal.jsx
@@ -1,53 +1,76 @@
 const {
-   ui: { ModalRoot, ModalHeader, ModalBody, ModalConfirmFooter, ModalSizes, TextArea },
-   solid: { createSignal },
-   plugin: { store },
+  ui: {
+    ModalRoot,
+    ModalHeader,
+    ModalBody,
+    ModalConfirmFooter,
+    ModalSizes,
+    TextArea,
+  },
+  solid: { createSignal },
+  plugin: { store },
 } = shelter;
 
 export function AddTagModal(closeModal, gifData) {
-   const [tags, setTags] = createSignal("");
+  const [tags, setTags] = createSignal("");
 
-   let tagsArray = store[gifData.url] || [];
-   setTags(tagsArray.join(", "));
+  let tagsArray = store[gifData.url] || [];
+  setTags(tagsArray.join(", "));
 
-   return (
-      <ModalRoot size={ModalSizes.MEDIUM}>
-         <ModalHeader close={closeModal}>Adding tags to GIF</ModalHeader>
-         <ModalBody>
-            <div
-               style={{
-                  display: "flex",
-                  "justify-content": "center",
-                  "align-items": "center",
-                  "margin-bottom": "16px",
-               }}
-            >
-               <video
-                  src={gifData.src}
-                  autoplay
-                  loop
-                  muted
-                  style={{
-                     width: "300px",
-                     height: "300px",
-                     "object-fit": "stretch",
-                     "border-radius": "8px",
-                  }}
-               />
-            </div>
-            <TextArea placeholder="Enter tags separated by commas" value={tags()} onInput={(text) => setTags(text)} />
-         </ModalBody>
-         <ModalConfirmFooter
-            close={closeModal}
-            confirmText="Save"
-            onConfirm={() => {
-               tagsArray = tags()
-                  .split(",")
-                  .map((tag) => tag.trim());
-               store[gifData.url] = tagsArray;
-               closeModal();
-            }}
-         />
-      </ModalRoot>
-   );
+  const isMP4 = gifData.src.endsWith(".mp4");
+
+  return (
+    <ModalRoot size={ModalSizes.MEDIUM}>
+      <ModalHeader close={closeModal}>Adding tags to GIF</ModalHeader>
+      <ModalBody>
+        <div
+          style={{
+            display: "flex",
+            "justify-content": "center",
+            "align-items": "center",
+            "margin-bottom": "16px",
+          }}
+        >
+          {isMP4 ? (
+            <video
+              src={gifData.src}
+              autoplay
+              loop
+              muted
+              style={{
+                "max-width": "100%",
+                "max-height": "300px",
+                "border-radius": "8px",
+              }}
+            />
+          ) : (
+            <img
+              src={gifData.src}
+              style={{
+                "max-width": "100%",
+                "max-height": "300px",
+                "border-radius": "8px",
+              }}
+            />
+          )}
+        </div>
+        <TextArea
+          placeholder="Enter tags separated by commas"
+          value={tags()}
+          onInput={(text) => setTags(text)}
+        />
+      </ModalBody>
+      <ModalConfirmFooter
+        close={closeModal}
+        confirmText="Save"
+        onConfirm={() => {
+          tagsArray = tags()
+            .split(",")
+            .map((tag) => tag.trim());
+          store[gifData.url] = tagsArray;
+          closeModal();
+        }}
+      />
+    </ModalRoot>
+  );
 }
