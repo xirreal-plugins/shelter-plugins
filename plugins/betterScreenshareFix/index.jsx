5bbbb6456ff630296fc93bc9ee042a2ec4248ceb
xirreal <me@xirreal.dev>
2025-05-22 10:06:16 +0200

    Format

--- plugins/betterScreenshareFix/index.jsx
+++ plugins/betterScreenshareFix/index.jsx
@@ -1,69 +1,67 @@
 const {
-  flux: { subscribe },
-  observeDom,
+   flux: { subscribe },
+   observeDom,
 } = shelter.plugin.scoped;
 
 const {
-  flux: {
-    stores: { UserStore },
-  },
-  ui: { ReactiveRoot },
-  solid: { onCleanup },
+   flux: {
+      stores: { UserStore },
+   },
+   ui: { ReactiveRoot },
+   solid: { onCleanup },
 } = shelter;
 
 let originalPremiumType = null;
 
 function patchNiterState() {
-  const user = UserStore.getCurrentUser();
-  const premiumType = user.premiumType || 0;
-  if (originalPremiumType === null) {
-    originalPremiumType = premiumType;
-  }
-  user.premiumType = 2;
+   const user = UserStore.getCurrentUser();
+   const premiumType = user.premiumType || 0;
+   if (originalPremiumType === null) {
+      originalPremiumType = premiumType;
+   }
+   user.premiumType = 2;
 }
 
 function restoreNiterState() {
-  const user = UserStore.getCurrentUser();
-  if (originalPremiumType === null) {
-    return;
-  }
-  user.premiumType = originalPremiumType === 0 ? null : originalPremiumType;
-  originalPremiumType = null;
+   const user = UserStore.getCurrentUser();
+   if (originalPremiumType === null) {
+      return;
+   }
+   user.premiumType = originalPremiumType === 0 ? null : originalPremiumType;
+   originalPremiumType = null;
 }
 
 function CleanupListener() {
-  onCleanup(() => {
-    restoreNiterState();
-  });
+   onCleanup(() => {
+      restoreNiterState();
+   });
 
-  return <div style={{ display: "none" }} />;
+   return <div style={{ display: "none" }} />;
 }
 
 export function onLoad() {
-  subscribe("TRACK", (e) => {
-    if (
-      e.event === "impression_go_live_modal" ||
-      (e.event == "open_modal" && e.properties.type == "Go Live Modal") ||
-      e.event === "impression_call_tile_context_menu"
-    ) {
-      const searchQuery =
-        e.event == "impression_call_tile_context_menu"
-          ? "#stream-context"
-          : "[class^='focusLock'] > div";
-      patchNiterState();
-      const stopObserving = observeDom(searchQuery, (node) => {
-        stopObserving();
-        node.parentNode.append(
-          <ReactiveRoot>
-            <CleanupListener />
-          </ReactiveRoot>
-        );
-      });
-      setTimeout(stopObserving, 500);
-    }
-  });
+   subscribe("TRACK", (e) => {
+      if (
+         e.event === "impression_go_live_modal" ||
+         (e.event == "open_modal" && e.properties.type == "Go Live Modal") ||
+         e.event === "impression_call_tile_context_menu"
+      ) {
+         const searchQuery =
+            e.event == "impression_call_tile_context_menu" ? "#stream-context" : "[class^='focusLock'] > div";
+         patchNiterState();
+         const stopObserving = observeDom(searchQuery, (node) => {
+            stopObserving();
+            node.parentNode.append(
+               <ReactiveRoot>
+                  <CleanupListener />
+               </ReactiveRoot>,
+            );
+         });
+         setTimeout(stopObserving, 500);
+      }
+   });
 }
 
 export function onUnload() {
-  restoreNiterState();
+   restoreNiterState();
 }
--- plugins/favoriteSearch/index.jsx
+++ plugins/favoriteSearch/index.jsx
@@ -1,110 +1,95 @@
 const {
-  flux: { subscribe },
-  observeDom,
+   flux: { subscribe },
+   observeDom,
 } = shelter.plugin.scoped;
 
 const {
-  ui: { TextBox, openModal },
-  util: { getFiberOwner, getFiber, reactFiberWalker },
-  plugin: { store },
+   ui: { TextBox, openModal },
+   util: { getFiberOwner, getFiber, reactFiberWalker },
+   plugin: { store },
 } = shelter;
 
 import { AddTagModal } from "./modal";
 
 function addClickHandlerToFavoritesGifPicker() {
-  const stopObserving = observeDom(
-    "[class^='result'] > [class^='categoryFadeBlurple']",
-    (container) => {
+   const stopObserving = observeDom("[class^='result'] > [class^='categoryFadeBlurple']", (container) => {
       stopObserving();
       if (container.parentElement.dataset.clickHandlerAdded) return;
 
       container.parentElement.dataset.clickHandler = "true";
       container.parentElement.addEventListener("click", handleClick);
-    }
-  );
-  setTimeout(stopObserving, 500);
+   });
+   setTimeout(stopObserving, 500);
 }
 
 let stopObservingResults = null;
 function handleClick() {
-  requestAnimationFrame(() => {
-    const header = document.querySelector(
-      "#gif-picker-tab-panel > div:first-child"
-    );
-    if (!header) return;
-    const searchBox = (
-      <TextBox
-        id="gif-searchbox"
-        style={{ "margin-top": "8px" }}
-        placeholder="Search favorite GIFs..."
-        onInput={(text) => handleSearch(text)}
-      />
-    );
-    header.appendChild(searchBox);
+   requestAnimationFrame(() => {
+      const header = document.querySelector("#gif-picker-tab-panel > div:first-child");
+      if (!header) return;
+      const searchBox = (
+         <TextBox
+            id="gif-searchbox"
+            style={{ "margin-top": "8px" }}
+            placeholder="Search favorite GIFs..."
+            onInput={(text) => handleSearch(text)}
+         />
+      );
+      header.appendChild(searchBox);
 
-    stopObservingResults = observeDom(
-      "[class^='content'] > div > [class^='result_']",
-      (card) => {
-        if (card.dataset.addedRightClick) return;
-        card.dataset.addedRightClick = "true";
+      stopObservingResults = observeDom("[class^='content'] > div > [class^='result_']", (card) => {
+         if (card.dataset.addedRightClick) return;
+         card.dataset.addedRightClick = "true";
 
-        addRightClickHandler(card);
-      }
-    );
-  });
+         addRightClickHandler(card);
+      });
+   });
 }
 
 function handleBack() {
-  document.getElementById("gif-searchbox")?.remove();
-  if (stopObservingResults) {
-    stopObservingResults();
-    stopObservingResults = null;
-  }
-  addClickHandlerToFavoritesGifPicker();
+   document.getElementById("gif-searchbox")?.remove();
+   if (stopObservingResults) {
+      stopObservingResults();
+      stopObservingResults = null;
+   }
+   addClickHandlerToFavoritesGifPicker();
 }
 
 function handleSearch(text) {
-  const container = document.querySelector("#gif-picker-tab-panel");
-  const fiber = getFiberOwner(container);
-  if (!fiber.props.__favorites) {
-    fiber.props.__favorites = fiber.props.favorites;
-  }
+   const container = document.querySelector("#gif-picker-tab-panel");
+   const fiber = getFiberOwner(container);
+   if (!fiber.props.__favorites) {
+      fiber.props.__favorites = fiber.props.favorites;
+   }
 
-  fiber.props.favorites = fiber.props.__favorites.filter((gif) => {
-    for (const tag of store[gif.url] ?? []) {
-      if (tag.toLowerCase().includes(text.toLowerCase())) {
-        return true;
+   fiber.props.favorites = fiber.props.__favorites.filter((gif) => {
+      for (const tag of store[gif.url] ?? []) {
+         if (tag.toLowerCase().includes(text.toLowerCase())) {
+            return true;
+         }
       }
-    }
 
-    return gif.url.toLowerCase().includes(text.toLowerCase());
-  });
+      return gif.url.toLowerCase().includes(text.toLowerCase());
+   });
 
-  fiber.forceUpdate();
+   fiber.forceUpdate();
 }
 
 function addRightClickHandler(container) {
-  container.addEventListener("contextmenu", (e) => {
-    console.log(
-      reactFiberWalker(getFiber(e.target.parentNode), "item", true)
-        .memoizedProps
-    );
-    openModal((p) =>
-      AddTagModal(
-        p.close,
-        reactFiberWalker(getFiber(e.target.parentNode), "item", true)
-          .memoizedProps.item
-      )
-    );
-  });
+   container.addEventListener("contextmenu", (e) => {
+      console.log(reactFiberWalker(getFiber(e.target.parentNode), "item", true).memoizedProps);
+      openModal((p) =>
+         AddTagModal(p.close, reactFiberWalker(getFiber(e.target.parentNode), "item", true).memoizedProps.item),
+      );
+   });
 }
 
 export function onLoad() {
-  subscribe("GIF_PICKER_INITIALIZE", (e) => {
-    addClickHandlerToFavoritesGifPicker();
-  });
+   subscribe("GIF_PICKER_INITIALIZE", (e) => {
+      addClickHandlerToFavoritesGifPicker();
+   });
 
-  subscribe("GIF_PICKER_QUERY", (e) => {
-    handleBack();
-  });
+   subscribe("GIF_PICKER_QUERY", (e) => {
+      handleBack();
+   });
 }
